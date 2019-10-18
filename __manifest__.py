c11a2eaf6a91b4e2a07867a97f21079cfea0492f
m66@euroblaze.de <m66@euroblaze.de>
2019-10-18 08:33:40 +0200

    Changed website in manifest

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
        Odoo module for connecting with any Woo Commerce Shop. 
     """,
     'author': "simplify-erp",
-    'website': "http://www.simplify-erp.com",
+    'website': "https://www.simplify-erp.com/shops/woocommerce/",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
