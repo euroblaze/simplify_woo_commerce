ce6cd315f9ea4cb37c0f1476007353700d860fd0
m66 <m66@euroblaze.de>
2020-04-03 11:47:50 +0200

    Added new field for Woo Status in Sale Order

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class InhertSaleOrder(models.Model):
     _inherit = 'sale.order'
 
     woo_order_number = fields.Integer(string='Woo Order Number')
     woo_order_key = fields.Char(string='Woo Order key')
+    woo_order_status = fields.Selection([('pending', 'Pending Payment'),
+                                         ('processing', 'Processing'),
+                                         ('on-hold', 'On hold'),
+                                         ('completed', 'Completed'),
+                                         ('cancelled', 'Cancelled'),
+                                         ('refunded', 'Refunded'),
+                                         ('failed', 'Failed'),
+                                         ('trash', 'Trash')
+                                         ],
+                                        default='pending',
+                                        description="Order status. Options: pending, processing, on-hold, completed, "
+                                                    "cancelled, refunded, failed and trash. Default is pending"
+                                        )
 
     def _compute_pos(self):
         if self.channel_id:
@@ -13,7 +27,6 @@ class InhertSaleOrder(models.Model):
             print("POS", self.pos)
 
     pos = fields.Integer(string='Channel pos', compute=_compute_pos, readonly=True)
-
 
     # @api.model
     # def create(self,vals):
@@ -26,5 +39,3 @@ class InhertSaleOrder(models.Model):
     #     res = super(InhertSaleOrder, self).write(values)
     #     res.action_confirm()
     #     return res
-
-
