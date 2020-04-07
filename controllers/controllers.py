1aaea92da59fc116bcc210ad68f6fab28a340718
Kristijan <m44@eueroblaze.de>
2020-04-07 15:04:14 +0200

    Updated controller.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -9,13 +9,8 @@ class WooCreateOrders(http.Controller):
     def create_product(self, **kwargs):
         _logger.info("===================================================WEBHOOK REQUEST ==================================================")
         _logger.info(http.request.jsonrequest)
-        _logger.info("============ID================")
-        _logger.info(http.request.jsonrequest["id"])
-        _logger.info("=========SELF=========")
+        _logger.info("==========SELF=========")
         _loggwe.info(self)
         return "Hello World!"
         # new_product = http.request.env['product.template'].sudo().create(kwargs)
-        # if new_product:
-        #     return "New product has been created"
-        # else:
-        #     return "No product created"
+       
