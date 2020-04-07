4a597b0dd6b3d8c0d67b3ee702f2e7d1a91104e4
m66 <m66@euroblaze.de>
2020-04-07 13:43:56 +0200

    Changed endpoint type

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+import logging
+_logger = logging.getLogger(__name__)
 
 class WooCreateOrders(http.Controller):
 
-    @http.route('/api/create_order', auth='public', csrf=False, type='json', methods=['POST'])
+    @http.route('/api/create_order', auth='public', csrf=False, type='http', methods=['POST'])
     def create_product(self, **kwargs):
-        print("====================================kwargs", kwargs)
+        _logger.info("====================================kwargs=====================")
+        _logger.info(kwargs)
+        _logger.info("/*/*/*/*/*/*/*HTTP/*/*/*/*/*/*/*/*/")
+        _logger.info(http.request.body)
         return "Hello World!"
         # new_product = http.request.env['product.template'].sudo().create(kwargs)
         # if new_product:
