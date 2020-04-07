0f26402c1bc9e8d400397cefc1c4de2d8fd537dd
m66 <m66@euroblaze.de>
2020-04-07 12:35:44 +0200

    Testing odoo endpoints

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class CustomAddons/simplifyWooCommerce(http.Controller):
-#     @http.route('/custom_addons/simplify_woo_commerce/custom_addons/simplify_woo_commerce/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class WooCreateOrders(http.Controller):
 
-#     @http.route('/custom_addons/simplify_woo_commerce/custom_addons/simplify_woo_commerce/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('custom_addons/simplify_woo_commerce.listing', {
-#             'root': '/custom_addons/simplify_woo_commerce/custom_addons/simplify_woo_commerce',
-#             'objects': http.request.env['custom_addons/simplify_woo_commerce.custom_addons/simplify_woo_commerce'].search([]),
-#         })
-
-#     @http.route('/custom_addons/simplify_woo_commerce/custom_addons/simplify_woo_commerce/objects/<model("custom_addons/simplify_woo_commerce.custom_addons/simplify_woo_commerce"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('custom_addons/simplify_woo_commerce.object', {
-#             'object': obj
-#         })
+    @http.route('/api/create_order', auth='public', csrf=False, type='json', methods=['POST'])
+    def create_product(self, **kwargs):
+        print("====================================kwargs", kwargs)
+        return "Hello World!"
+        # new_product = http.request.env['product.template'].sudo().create(kwargs)
+        # if new_product:
+        #     return "New product has been created"
+        # else:
+        #     return "No product created"
