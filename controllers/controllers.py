5df85053381628abf99776d1b8699c630a924566
Kristijan <m44@eueroblaze.de>
2020-04-07 14:58:20 +0200

    Changed the contoller for receiving wc webhook

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,12 +5,14 @@ _logger = logging.getLogger(__name__)
 
 class WooCreateOrders(http.Controller):
 
-    @http.route('/api/create_order', auth='public', csrf=False, type='http', methods=['POST'])
+    @http.route('/api/create_order', auth='public', csrf=False, type='json', methods=['POST'])
     def create_product(self, **kwargs):
-        _logger.info("====================================kwargs=====================")
-        _logger.info(kwargs)
-        _logger.info("/*/*/*/*/*/*/*HTTP/*/*/*/*/*/*/*/*/")
-        _logger.info(http.request.body)
+        _logger.info("===================================================WEBHOOK REQUEST ==================================================")
+        _logger.info(http.request.jsonrequest)
+        _logger.info("============ID================")
+        _logger.info(http.request.jsonrequest["id"])
+        _logger.info("=========SELF=========")
+        _loggwe.info(self)
         return "Hello World!"
         # new_product = http.request.env['product.template'].sudo().create(kwargs)
         # if new_product:
