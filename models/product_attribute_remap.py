e81fc453ebae35e19d43f606940e6020e21ed636
m66 <m66@euroblaze.de>
2020-06-08 15:27:59 +0200

    Update product.template view

--- models/product_attribute_remap.py
+++ models/product_attribute_remap.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class ProductAttributeReMap(models.Model):
     _name = 'product.attribute.remap'
 
-    original_value_id = fields.Many2one('product.attribute.value', string='Original Color', ondelete='restrict')
+    original_value_id = fields.Many2one('product.attribute.value', string='Original Value', ondelete='restrict')
     remapped_value_ids = fields.Many2many('product.attribute.value', string='Remapped Values')
     channel_id = fields.Many2one('channel.pos.settings', string='Channel Instance ID',
                                      description="Woo Channel instance ID")
